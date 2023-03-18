2bbbd397c3f2993e308b7f4901edd2deb41d902c
anhtu-pham <122914252+anhtu-pham@users.noreply.github.com>
2023-03-18 13:12:10 -0400

    added new Expo SDK

--- android/app/src/main/java/com/fooddelivery/MainActivity.java
+++ android/app/src/main/java/com/fooddelivery/MainActivity.java
@@ -1,3 +1,4 @@
+import expo.modules.ReactActivityDelegateWrapper;
 package com.fooddelivery;
 
 import android.os.Bundle;
@@ -28,7 +29,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new MainActivityDelegate(this, getMainComponentName());
+    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new MainActivityDelegate(this, getMainComponentName()));
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
