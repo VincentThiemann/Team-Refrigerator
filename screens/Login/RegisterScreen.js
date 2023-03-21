829845722f36f8a7a8fd94520e967a946c23c059
anhtu-pham <122914252+anhtu-pham@users.noreply.github.com>
2023-03-21 15:27:02 -0400

    Update

---
+++ screens/Login/RegisterScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const RegisterScreen = () => {
+    return (
+        <View style={style.container}>
+            <InputData keyboardType = "default" type = "First name:" placeholder = "Type first name here" />
+            <InputData keyboardType = "default" type = "Last name:" placeholder = "Type last name here" />
+            <InputData keyboardType = "default" type = "Username:" placeholder = "Type username here" />
+            <InputData keyboardType = "email-address" type = "Email:" placeholder = "Type email here" />
+            <InputData keyboardType = "phone-pad" type = "Phone number:" placeholder = "Type phone number here" />
+            <InputData keyboardType = "default" type = "Password:" placeholder = "Type password here" secureTextEntry = {true}/>
+        </View>
+    )
+}
+export const InputData = (props) => {
+    return (
+        <View style={style.container}>
+            <Text>{props.type}</Text>
+            <TextInput style = {styles.input} placeholder = {props.placeholder} keyboardType = {props.dataType}/>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    input: {
+        borderRadius: 12,
+        backgroundColor: COLORS.green,
+        padding: 15
+    }
+})
