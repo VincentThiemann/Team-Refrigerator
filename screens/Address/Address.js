a938d21410b681988c70a8e389b0689445cabc61
anhtu-pham <122914252+anhtu-pham@users.noreply.github.com>
2023-04-06 21:14:44 -0400

    Create Address.js

---
+++ screens/Address/Address.js
@@ -0,0 +1,46 @@
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 10,
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: SIZES.h1,
+        color: COLORS.black,
+        ...FONTS.h1,
+    },
+    text: {
+        textAlign: 'center',
+        color: COLORS.black,
+        ...FONTS.body1,
+    },
+    input: {
+        height: 100,
+        marginHorizontal: 20,
+        borderWidth: 1,
+        borderRadius: 15,
+        fontSize: SIZES.h3,
+        padding: 15,
+        borderWidth: 1,
+        borderColor: COLORS.lightGray1,
+        backgroundColor: COLORS.lightGray1,
+    },
+    button: {
+        height: 60,
+        width: '80%',
+        paddingLeft: SIZES.radius,
+        borderRadius: 50,
+        backgroundColor: COLORS.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 15,
+    },
+    image: {
+        alignSelf: 'center',
+        width: Display.setWidth(80),
+        height: Display.setHeight(40),
+        borderRadius: SIZES.padding,
+    }
+})
