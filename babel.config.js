a20d209bd784e7d3690af7f8ebf4e67a6dae939b
archspire-9k <leminhduc6122003@gmail.com>
2023-04-11 14:26:50 -0400

    added support for secret environment variables

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,16 @@ module.exports = {
         'react-native-reanimated/plugin', {
             relativeSourceLocation: true,
         },
-    ]
+    ],
+    ["module:react-native-dotenv", {
+      "envName": "APP_ENV",
+      "moduleName": "@env",
+      "path": ".env",
+      "blocklist": null,
+      "allowlist": null,
+      "safe": false,
+      "allowUndefined": true,
+      "verbose": false
+    }]
   ],
 };
--- firebase.js
+++ firebase.js
@@ -1,17 +1,27 @@
+import {
+FIREBASE_API_KEY,
+FIREBASE_AUTH_DOMAIN,
+FIREBASE_MESSAGER_SENDER_ID,
+FIREBASE_PROJECT_ID,
+FIREBASE_STORAGE_BUCKET,
+FIREBASE_APP_ID
+} from "@env"
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyDjaQF5fyf85izyYBlUNfLGdUrSHShlybM",
-    authDomain: "fir-auth-60b56.firebaseapp.com",
-    projectId: "fir-auth-60b56",
-    storageBucket: "fir-auth-60b56.appspot.com",
-    messagingSenderId: "675071634893",
-    appId: "1:675071634893:web:1358a051453765069f6884"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGER_SENDER_ID,
+    appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
