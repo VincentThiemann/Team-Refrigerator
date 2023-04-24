c0cdb49bfd20bfbf5f3263821d9c35d222859650
archspire-9k <leminhduc6122003@gmail.com>
2023-04-24 10:34:58 -0400

    add bookmark state

--- stores/rootReducer.js
+++ stores/rootReducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from "redux";
 import tabReducer from "./tabs/tabReducer";
 import firstLaunchReducer from "./firstLaunch/firstLaunchReducer";
+import cartReducer from './cart/cartReducer';
+import bookmarkReducer from './bookmark/bookmarkReducer';
+
 
 export default combineReducers({
-    tabReducer, 
-    generalState: firstLaunchReducer
+    tabReducer,
+    generalState: firstLaunchReducer,
+    cartState: cartReducer,
+    bookmarkState: bookmarkReducer,
+
 })
