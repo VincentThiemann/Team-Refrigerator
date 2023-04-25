21a9e8db34bba57dbf079c3ddf2583971fbf451a
anhtu-pham <122914252+anhtu-pham@users.noreply.github.com>
2023-04-24 20:09:50 -0400

    Update on work for NotFound and Search

--- screens/Search/Search.js
+++ screens/Search/Search.js
@@ -58,7 +58,7 @@ const Search = () => {
                         }}
                         onFail={error => console.log(error)}
 
-                        // Show sad emoji if location not found
+                        // If not found, just print error message
                         onNotFound={() => console.log("No Result")}
                         // listEmptyComponent={() => (
                         //     <NotFound />
@@ -125,20 +125,6 @@ const Search = () => {
             // </TouchableWithoutFeedback>
         // </KeyboardAvoidingView>
     )
-}
-
-const NotFound = () => {
-    const [loading, setLoading] = React.useState(true)
-    React.useEffect(() => { setTimeout(() => setLoading(false), 1000) }, []);
-    if (!loading) {
-        return (
-            <View style={{ alignItems: 'center', justifyContent: 'center', marginVertical: 50 }}>
-                <Image style={{ width: 300, height: 300 }} source={require("../../assets/images/sad.png")} />
-                <View style={{ marginVertical: 20 }} />
-                <Text style={styles.title}>Could not find the requested address</Text>
-            </View>
-        )
-    }
 }
 
 const styles = StyleSheet.create({
